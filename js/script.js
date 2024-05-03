76c3bcb346cbafd1d7e7b903cb9682057b5fca25
Nestor Melendez <melendezinv4321@gmail.com>
2024-05-03 10:33:11 -0400

    lis

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@
   $form.addEventListener("submit", (e) => {
     e.preventDefault();
     $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/melendezinv4321@gmail.com", {
+    fetch("https://formsubmit.co/ajax/fc726b8da72a21fba5e5197437507eC2", {
       method: "POST",
       body: new FormData(e.target),
     })
